cc365ac01ac64e59bf7ddc56cbf140949151400f
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-08-28 15:33:47 -0400

    wrap: use getter to expose state, & a setter version.
    I think this code needs to be reshuffled to deal with the whateverness
    that is decorators bizzarre new invented scope nonsense but this
    should be a clear enough idea of what is wanted. Do I have to unroll
    & duplicate code? Can I share?

--- wrap.js
+++ wrap.js
@@ -1,21 +1,54 @@
 "use module"
 
-export decorator @MemoizeGetSet{
-	@wrap( fn=> {
-		// only wrap get functions
-		if( !fn.name.startsWith( "get ")){
-			return fn
-		}
-		let value
-		function memoized(){
-			if( fn=== null){
-				return value
-			}
-			fn= null // free
-			value= fn.call( this)
+function wrapGet( fn){
+	function memoized(){
+		if( memoized.fn=== null){
 			return value
 		}
-		memoized.name= fn.name
-		return memoized
+		memoized.reset= function( fn){
+			memoized.fn= fn
+		}
+		memoized.fn= null // free
+		memoized.value= fn.call( this)
+		return memoized.value
+	}
+	return memoized
+}
+
+function wrapSet( fn){
+	function memoized( newValue){
+		if( !memoized.getter){
+			memoized.getter= Object.getOwnPropertyDescriptor( this, memoized.shortName).get
+		}
+		memoized.getter.value= newValue
+		memoized.getter.fn= null
+	}
+	const
+	  nameIsSet= fn.name.startsWith( "set"),
+	  shortName= nameIsSet? fn.name.substring( 4): fn.name
+	memoized.shortName= shortName
+	return memoized
+}
+
+export decorator @MemoizeGetSet{
+	@wrap( fn=> {
+		if( fn.name.startsWith( "get ")){
+			// wrap get functions
+			return wrapGet( fn)
+		}
+		return fn
+	})
+}
+
+export decorator @MemoizeGetSetSettable{
+	@wrap( fn=> {
+		if( fn.name.startsWith( "get ")){
+			// wrap get functions
+			return wrapGet( fn)
+		}else if( fn.name.startsWith( "set ")){
+			// wrap set functions
+			return wrapSet( fn)
+		}
+		return fn
 	})
 }
